279610fb66c2d291140ad97018962f0f0113a55f
Abraham Luna Cazares <L17550824@chihuahua2.tecnm.mx>
2020-04-24 10:04:20 -0600

    Pantalla principal terminada

---
+++ app/src/main/java/iceCreamSandwich/unidad2/examenpractico2icecreamsandwich/DatosActivity.java
@@ -0,0 +1,14 @@
+package iceCreamSandwich.unidad2.examenpractico2icecreamsandwich;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+    }
+}
---
+++ app/src/main/java/iceCreamSandwich/unidad2/examenpractico2icecreamsandwich/MainActivity.java
@@ -0,0 +1,37 @@
+package iceCreamSandwich.unidad2.examenpractico2icecreamsandwich;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView capturar;
+    LinearLayout textLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textLayout = findViewById(R.id.textLayout);
+
+        capturar = findViewById(R.id.capturar);
+        capturar.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.capturar:
+                startActivity(new Intent(this, DatosActivity.class));
+                break;
+        }
+    }
+}
